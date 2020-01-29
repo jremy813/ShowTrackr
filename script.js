5e3e52d1914c7c7a739e29876a88018e20014653
jremy813 <jremywatkins@gmail.com>
2020-01-29 00:41:19 -0500

    updated animation to bounce effect with javascript and added smooth scroll to sections

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //Scripts
-gsap.from("header", { duration: 0.6, ease: "power2. out", y: -60 });
+gsap.from("header", { duration: 0.9, ease: "bounce", y: -60 });
 gsap.from("#cta", {
-  duration: 0.6,
-  ease: "power2. out",
+  duration: 0.9,
+  ease: "bounce",
   opacity: 0,
   y: -40,
   delay: 1
