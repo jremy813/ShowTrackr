85f4069855d12d282388b537bc0517fbdbae0a97
jremy813 <jremywatkins@gmail.com>
2020-01-28 23:00:14 -0500

    added a little bit of animation with gsap

---
+++ script.js
@@ -0,0 +1,9 @@
+//Scripts
+gsap.from("header", { duration: 0.6, ease: "power2. out", y: -60 });
+gsap.from("#cta", {
+  duration: 0.6,
+  ease: "power2. out",
+  opacity: 0,
+  y: -40,
+  delay: 1
+});
